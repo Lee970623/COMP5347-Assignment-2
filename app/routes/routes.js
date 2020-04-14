44967e8f4a3a9516746e3e175db4d139811a142a
chli4002 <chli4002@uni.sydney.edu.au>
2020-04-15 00:24:57 +1000

    login and sign-up JS in progress

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,9 +3,11 @@ var controller = require('../controllers/controllers.js')
 var router = express.Router()
 
 router.get('/', controller.showLoginPage);
-// router.post('/signin', controller.signIn);
-// router.post('/signup', controller.signUp);
+router.post('/signin', controller.signIn);
+router.post('/signup', controller.signUp);
 
 router.get('/reset', controller.resetPwd);
+
+router.get('/main', controller.mainPageTest);
 
 module.exports = router;
---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+var el = $('.tabs').first()[0];
+var instance = M.Tabs.init(el);
+
+$(document).ready(function () {
+    $('select').formSelect(); // TODO: 这是哪个方法？
+    $('#signin').click(varifySignin);
+    $('#signup').click(signUp);
+});
+
+//Varify input from sign-in page
+function varifySignin() {
+    // TODO: Verify input data
+
+    var formdata = {
+        email: $('#email').val(),
+        password: $('#password').val()
+    }
+
+    console.log(formdata); // Test line
+
+    // Post form data to contorller
+    $.ajax({
+        type: 'POST',
+        url: '/signin',
+        dataType: 'JSON',
+        data: formdata,
+        success: function (res) {
+
+            alert(JSON.stringify(res)) //Test line.
+
+            if (res.loginStatus) {
+                window.location.href = '/main'
+            } else {
+                alert("Wrong username or password.")
+                //TODO: Change to toast
+            }
+        }
+    })
+}
+
+// Sign-up
+function signUp() {
+    //TODO: Verify input from sign-up page
+
+    var formdata = {
+        email: $('#re_email').val(),
+        password: $('#re_password').val(),
+        firstname: $('#firstname').val(),
+        lastname: $('#lastname').val(),
+        question: $('#questions').val(),
+        answer: $('#answer').val(),
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/signup',
+        dataType: 'JSON',
+        data: formdata,
+        success: function (res) {
+            alert(JSON.stringify(res));//Test line.
+
+            if (res.registerStatus) {
+                window.location.href = '/main'
+            } else {
+                //TODO: Materialize toast
+                alert('Sign-up failed.')
+            }
+        }
+    });
+}
