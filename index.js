22fbf309d3e2d2705b093a4a2b3b80e466f83009
Zidong Li <zili9346@uni.sydney.edu.au>
2020-04-14 17:50:23 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var routes = require('./app/routes/routes')
 
-//initial setting
+// initial setting
 var app = express();
-app.set('views',path.join(__dirname,'/app/views'));
-app.use(session({secret:'secretMessage', cookie:{maxAge:600000},resave: true, saveUninitialized: true}));
+app.set('views', path.join(__dirname, '/app/views'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secretMessage', cookie: { maxAge: 600000 }, resave: true, saveUninitialized: true }));
+app.use(express.static(path.join(__dirname, '/public')))
 
+// routes
+app.use('/', routes);
 
-
-
-//routes
-app.use('/',routes);
-app.listen(3000,function(){
+app.listen(3000, function() {
     console.log('listening on port 3000\n');
 })
