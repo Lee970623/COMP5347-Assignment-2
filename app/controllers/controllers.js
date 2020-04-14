e6885df6ac5b2625d876b369a182d683386e316f
chli4002 <chli4002@uni.sydney.edu.au>
2020-04-15 00:24:57 +1000

    login and sign-up JS in progress

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,3 +1,6 @@
+var model = require('../models/models')
+var crypto = require('crypto');
+
 // show login page
 function showLoginPage(req, res) {
     res.render("index.ejs");
@@ -7,12 +10,38 @@ function resetPwd(req, res) {
     res.render("reset.ejs");
 }
 
-// user login
-// function signIn(req, res)
+// User sign-in.
+function signIn(req, res) {
+    let loginstat = false;
 
-// user register
-// function signUp(req, res)
+    console.log("[received data]" + JSON.stringify(req.body)) // Test line.
 
+    //TODO: Send query to DB
+    loginstat = true;
+    req.session.loginStatus = loginstat;
+
+    // Return data to ajax method so that success callback can execute.
+    res.send({loginStatus: loginstat})
+}
+
+// User sign-up.
+function signUp(req, res) {
+    let regStat = false;
+
+    console.log("[receive data]" + JSON.stringify(req.body));
+
+    //TODO: Send query to DB
+    regStat = true;
+    req.session.loginStatus = regStat;
+
+    res.send({registerStatus: regStat})
+}
+
+
+// Test main page
+function mainPageTest(req, res) {
+    res.render("main.ejs");
+}
 
 // exports
-module.exports = { showLoginPage, resetPwd }
+module.exports = {showLoginPage, resetPwd, signIn, signUp, mainPageTest};
