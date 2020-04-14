741793273eb75ba1b43a4350e17cd6ce4c4eb1fb
chli4002 <chli4002@uni.sydney.edu.au>
2020-04-15 01:11:24 +1000

    update password hashing

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,9 +5,7 @@ var router = express.Router()
 router.get('/', controller.showLoginPage);
 router.post('/signin', controller.signIn);
 router.post('/signup', controller.signUp);
-
 router.get('/reset', controller.resetPwd);
-
 router.get('/main', controller.mainPageTest);
 
 module.exports = router;
